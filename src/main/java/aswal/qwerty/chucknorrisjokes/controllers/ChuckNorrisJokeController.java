ada61306ac22a9c7218b19a7f5f8280017fb01a0
Shubham Aswal <shubham@hacklab.in>
2021-03-16 12:18:39 +0530

    Chuck norris jokes controller errors fixed. Index Page added. Thymeleaf Frontend created. Assignment Complete. Closes #5.

--- src/main/java/aswal/qwerty/chucknorrisjokes/controllers/ChuckNorrisJokeController.java
+++ src/main/java/aswal/qwerty/chucknorrisjokes/controllers/ChuckNorrisJokeController.java
@@ -8,15 +8,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class JokesController {
+public class ChuckNorrisJokeController {
     private final JokeService joke;
 
     @Autowired
-    public JokesController(@Qualifier("chuckNorrisJokeService") JokeService joke) {
+    public ChuckNorrisJokeController(@Qualifier("chuckNorrisJokeService") JokeService joke) {
         this.joke = joke;
     }
 
-    @RequestMapping
+    @RequestMapping("/")
     public String getChuckNorrisJoke(Model model){
         model.addAttribute("joke",joke.getJoke());
         return "index";
